3e73cda9311c3967ebb93d55f3b2fb0742d4a64f
Joshim Uddin <joshimcse@gmail.com>
2021-01-23 19:12:51 +0600

    support eps to pdf formatter

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ global.__basedir = __dirname
 
 const fileType = require('./utils/fileType')
 const isImage = require('./utils/isImage')
-const { aiToPdf, epsToPdf, psdToPng } = require('./utils/converter')
+const { aiToPdf, epsToPdf, psdToPng, pngToPdf } = require('./utils/converter')
 const { readFiles } = require('./utils/filePath')
 
 fs.readdir(readFiles(), function (err, files) {
@@ -16,9 +16,10 @@ fs.readdir(readFiles(), function (err, files) {
 
   files.forEach((file) => {
     if (isImage(file)) {
-      console.log(
-        `${file} is a images type file, we still working for images to pdf converter...`
-      )
+      // console.log(
+      //   `${file} is a images type file, we still working for images to pdf converter...`
+      // )
+      pngToPdf(file)
     } else if (fileType(file) === 'ai') {
       aiToPdf(file)
     } else if (fileType(file) === 'eps') {
--- utils/converter.js
+++ utils/converter.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const psd = require('psd')
+var gm = require('gm').subClass({ imageMagick: true })
 const colors = require('colors')
 
 const { getFile, convertedFile } = require('./filePath')
@@ -20,30 +21,68 @@ exports.aiToPdf = (file) => {
 }
 
 exports.epsToPdf = (file) => {
-  fs.copyFile(
-    getFile(file),
-    convertedFile(includeExt(fileNameWithOutExt(file), 'pdf')),
-    (err) => {
-      if (!err) {
-        console.log(
-          colors.cyan(file) + ' converted as pdf to dist dir successfully'
-        )
+  gm(getFile(file))
+    .in('-colorspace')
+    .in('srgb')
+    .write(
+      convertedFile(includeExt(fileNameWithOutExt(file), 'pdf')),
+      function (err) {
+        if (!err) {
+          console.log(
+            colors.cyan(file) + ' converted as pdf to dist dir successfully'
+          )
+        } else {
+          console.log(err)
+        }
       }
-    }
-  )
+    )
 }
 
 exports.psdToPng = (file) => {
-  psd
-    .open(getFile(file))
-    .then(function (data) {
-      return data.image.saveAsPng(
-        convertedFile(includeExt(fileNameWithOutExt(file), 'png'))
-      )
-    })
-    .then(function () {
-      console.log(
-        colors.cyan(file) + ' converted as png to dist dir successfully'
-      )
-    })
+  // psd
+  //   .open(getFile(file))
+  //   .then(function (data) {
+  //     return data.image.saveAsPng(
+  //       convertedFile(includeExt(fileNameWithOutExt(file), 'png'))
+  //     )
+  //   })
+  //   .then(function () {
+  //     console.log(
+  //       colors.cyan(file) + ' converted as png to dist dir successfully'
+  //     )
+  //   })
+
+  gm(getFile(file))
+    .in('-colorspace')
+    .in('srgb')
+    .write(
+      convertedFile(includeExt(fileNameWithOutExt(file), 'pdf')),
+      function (err) {
+        if (!err) {
+          console.log(
+            colors.cyan(file) + ' converted as pdf to dist dir successfully'
+          )
+        } else {
+          console.log(err)
+        }
+      }
+    )
+}
+
+exports.pngToPdf = (file) => {
+  gm(getFile(file))
+    .in('-colorspace')
+    .in('srgb')
+    .write(
+      convertedFile(includeExt(fileNameWithOutExt(file), 'pdf')),
+      function (err) {
+        if (!err) {
+          console.log(
+            colors.cyan(file) + ' converted as pdf to dist dir successfully'
+          )
+        } else {
+          console.log(err)
+        }
+      }
+    )
 }
